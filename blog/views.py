31b66983bdaccb7a58d666dee3d06469d648d8dc
Amy Lin <linamy85@gmail.com>
2016-02-19 12:59:38 +0800

    can add new / edit article from website if logged in

--- blog/views.py
+++ blog/views.py
@@ -1,6 +1,7 @@
-from django.shortcuts import render , get_object_or_404
+from django.shortcuts import render , get_object_or_404 , redirect
 from django.utils import timezone
 from .models import Post
+from .forms import PostForm
 
 # Create your views here.
 
@@ -11,3 +12,33 @@ def post_list( req ):
 def post_detail( req , pk ):
     post = get_object_or_404( Post , pk=pk )
     return render( req , 'blog/post_detail.html' , { 'post' : post } )
+
+def post_new( req ):
+    if req.method == "POST":
+        form = PostForm( req.POST )
+        if form.is_valid():
+            post = form.save( commit = False ) # because we want to add author, so not commit first
+                                               # but most of the time, we commit it first
+            post.author = req.user
+            post.published_date = timezone.now()
+            post.save()
+            return redirect( 'blog.views.post_detail' , pk = post.pk )
+    else:
+        form = PostForm()
+    return render( req , 'blog/post_edit.html' , { 'form' : form } )
+
+def post_edit( req , pk ):
+    post = get_object_or_404( Post , pk=pk )
+    if req.method == "POST":
+        form = PostForm(req.POST, instance=post)
+        if form.is_valid():
+            post = form.save( commit = False )
+            post.author = req.user
+            post.published_date = timezone.now()
+            post.save()
+            return redirect( 'blog.views.post_detail' , pk=post.pk )
+    else:
+        form = PostForm( instance=post )
+    return render( req , 'blog/post_edit.html' , { 'form' : form } )
+
+
